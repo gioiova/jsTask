5a2ab0c573e13364a0814048ae4dc8b6172ee784
gioiova <iovashvili.gio11@gmail.com>
2024-10-12 16:57:58 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+	
+// js დავალება 1
+
+// 1 ) იპოვეთ ყველაზე დიდი რიცხვი მოცემულ მასივში let arr = [1, 3, 5, 7, 2];
+//let arr = [1, 3, 5, 7, 2]
+
+// let maxNum = Math.max(...arr)
+// console.log(maxNum)
+
+// 2) შეაბრუნეთ reverse მასივი let arr = [1, 2, 3, 4, 5];
+
+// let arr = [1, 2, 3, 4, 5];
+// let reverseArr = arr.reverse()
+// console.log(reverseArr)
+
+
+
+// 3) მოცემულ ერეიში დააჯამეთ მონაცემები let arr = [1, 2, 3, 4, 5];
+
+// let arr = [1, 2, 3, 4, 5]
+// var sum = 0 ;
+// for(let i = 0 ; i <arr.length;i++) {
+
+//     sum += arr[i];
+// }
+// console.log(sum)
+
+
+
+
+
+// 4) მოცემული ორი ერეიდან იპოვეთ მსგავსი რიცხვები let arr1 = [1, 2, 3, 4];
+// let arr2 = [3, 4, 5, 6];
+// let arr1 = [1, 2, 3, 4];
+// let arr2 = [3, 4, 5, 6];
+// let arr3 = arr1.concat(arr2)
+// const duplicates = arr3.filter((item, index) => arr3.indexOf(item) !== index);
+// console.log(duplicates)
+
+
+
+
+
+// 5)  შექმენით ფუნქცია სადაც ერთ ფუნქციას გამოიყენებთ მოერე ფუნქციის პარამეტრად და ifelse მეშვეობით დაბეჭდეთ რამე ინფორმაცია (მაგ: თუ რამე ნივთის ფასი
+// მაღაზიაში არის 50 ლარზე მეტი ესეიგი ძვირია თუ 20-50 დიაპაზონშია მოქცეული
+// საშუალო თუ ნაკლები იაფი)
+
+// function display(x,secondfunc){
+//     secondfunc(x)
+// }
+
+// function shefasebisfunc(price) {
+//     if (price > 50) {
+//         console.log("High");
+//     } else if (price > 20 && price <= 50) {
+//         console.log("Average");
+//     } else {
+//         console.log("Low");
+//     }
+// }
+
+// display (200,shefasebisfunc)
